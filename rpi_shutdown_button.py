d662a29d8262bd63123880be826382825eaba9bb
Your name <you@example.com>
2019-03-27 02:51:06 +0100

    Using 'wait_for_edge' instead of loop

--- rpi_shutdown_button.py
+++ rpi_shutdown_button.py
@@ -4,7 +4,8 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 """
-Place shutdown-button.service in /etc/systemd/system, then active by using systemctl enable shutdown-button.service.
+Place shutdown-button.service in /etc/systemd/system,
+then active by using systemctl enable shutdown-button.service.
 
 You also need to specify the path of the python script in shutdown-button.service,
 for example by using 'readlink -f <filename>'.
@@ -14,18 +15,14 @@ Otherwise you can store that path in a variable and call $VARIABLE in shutdown-b
 
 # config section
 input_pin_number = 18
-sleep_duration = 1  # in seconds
 should_print_to_console = True
 
 # initialization
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(input_pin_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(input_pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# main loop
-while True:
-    input_state = GPIO.input(input_pin_number)
-    if not input_state:
-        if should_print_to_console:
-            print('Shutdown initiated ...')
-        call("systemctl poweroff", shell=True)
-    sleep(sleep_duration)
+# block until rise in voltage is detected
+GPIO.wait_for_edge(input_pin_number, GPIO.RISING)
+    if should_print_to_console:
+        print('Shutdown initiated ...')
+call("systemctl poweroff", shell=True)
